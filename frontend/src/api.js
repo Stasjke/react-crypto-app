afe005e9950b26ef25af6ed9388061c5485c4748
Stanislav Ryazantsev <ryazancev_stas94@mail.ru>
2025-03-17 14:20:10 +0700

    init application

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,17 +1,16 @@
 import { cryptoAssets, cryptoData } from './data'
 
 export function fakeFetchCrypto() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(cryptoData)
-    }, 2000)
-  })
+	return new Promise(resolve => {
+		setTimeout(()=> {
+			resolve(cryptoData)
+		}, 1)
+	})
 }
-
 export function fetchAssets() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(cryptoAssets)
-    }, 2000)
-  })
+	return new Promise(resolve => {
+		setTimeout(()=> {
+			resolve(cryptoAssets)
+		}, 2000)
+	})
 }
